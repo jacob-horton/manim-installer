f8866d27831729f662687bf1ff91516aca1bbbc0
jacob-horton <50290314+jacob-horton@users.noreply.github.com>
2022-02-22 16:35:34 +0000

    Removed old comment

--- install.ps1
+++ install.ps1
@@ -5,7 +5,6 @@ $yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","De
 $no = New-Object System.Management.Automation.Host.ChoiceDescription "&No","Description."
 $options = [System.Management.Automation.Host.ChoiceDescription[]]($yes, $no)
 
-## Use the following each time your want to prompt the use
 $title = "Restart?" 
 $message = "You need to restart your computer, would you like to do that now? "
 $result = $host.ui.PromptForChoice($title, $message, $options, 1)
